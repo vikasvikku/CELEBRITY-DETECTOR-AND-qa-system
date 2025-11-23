ae4dbb694d3604967cd5dcb78d568f87db6310ed
vikas.k <vikas.k@wisework.in>
2025-11-23 16:29:50 +0530

    updadted init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask
+from dotenv import load_dotenv
 import os
 
 def create_app():
-    # Get the parent directory (project root)
-    basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    
-    app = Flask(__name__, 
-                template_folder=os.path.join(basedir, 'templates'),
-                static_folder=os.path.join(basedir, 'static'))
-    
+    load_dotenv()
+    template_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','templates'))
+
+    app = Flask(__name__ , template_folder=template_path)
+
+    app.secret_key = os.getenv("SECRET_KEY" , "default_secret")
+
     from app.routes import main
+
     app.register_blueprint(main)
-    
+
     return app
